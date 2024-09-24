99cfeb0700c0408ccc6dab32a837fc3a6e7a9bc7
Joao M Pacheco <jcoelhopacheco@gmail.com>
2024-09-23 22:13:18 -0300

    Iniciando aula Processamento de Dados

---
+++ src/reaprendendo/ProcessamentoDados.java
@@ -0,0 +1,16 @@
+package reaprendendo;
+
+public class ProcessamentoDados {
+
+	public static void main(String[] args) {
+		// Exemplo 1
+		int x,y;
+		x = 5;
+		y = 12 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
